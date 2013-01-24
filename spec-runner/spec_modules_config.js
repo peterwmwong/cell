e07dda7526d4240b384ac0eb65e50b2b0ea52ded
Peter Wong <peter.wm.wong@gmail.com>
2013-01-23 21:02:09 -0600

    update spec-runner to run on gh-pages

--- spec-runner/spec_modules_config.js
+++ spec-runner/spec_modules_config.js
@@ -1,6 +1,6 @@
 require.config({
   context: 'specRunner', // TODO rename spec-runner
-  baseUrl: '/spec-runner/',
+  baseUrl: '../spec-runner/',
   paths: {
     'SpecHelpers': '../spec-runner/SpecHelpers',
     'spec': '../spec-runner/spec',
